00dfd192b64446c7608dcca0a151c0f325b60e41
wittefeng <wittefeng@gmail.com>
2022-03-25 23:30:46 +0800

    feat:cors

--- config/config.default.js
+++ config/config.default.js
@@ -1,30 +1,36 @@
 /* eslint valid-jsdoc: "off" */
 
-'use strict';
+'use strict'
 
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
+module.exports = (appInfo) => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = (exports = {})
 
+  config.cors = {
+    // * 允许所有域名访问
+    origin: 'http://localhost:8001',
+    // 允许的方法
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  }
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1647353155883_599';
+  config.keys = appInfo.name + '_1647353155883_599'
 
   // add your middleware config here
-  config.middleware = [];
+  config.middleware = []
 
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
-  };
+  }
 
   return {
     ...config,
-    ...userConfig,
-  };
-};
+    ...userConfig
+  }
+}
